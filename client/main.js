59a114acf0b520b8d019d6de3b8d0a084844d041
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-11-22 18:21:29 -0700

    Make boilerplate React project with Mongo collection pulled from Meteor tutorial

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+ 
+import App from '../imports/ui/App.js';
+ 
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
