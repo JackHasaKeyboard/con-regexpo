5fb79ba4c74b1c125bd8f1e05c992111454f1612
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-11-22 23:28:27 -0700

    Add style and re-format

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
- 
+
 import { Tasks } from '../api/tasks.js';
- 
+
 import Task from './Task.js';
- 
+
 class App extends Component {
-  renderTasks() {
-    return this.props.tasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
- 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>RegEx Repo</h1>
-        </header>
- 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+	renderTasks() {
+		return this.props.tasks.map((task) => (
+			<Task key={task._id} task={task} />
+		));
+	}
+
+	render() {
+		return (
+			<div id="cont">
+				{this.renderTasks()}
+			</div>
+		);
+	}
 }
 
 export default withTracker(() => {
-  return {
-    tasks: Tasks.find({}).fetch(),
-  };
+	return {
+		tasks: Tasks.find({}).fetch(),
+	};
 })(App);
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -4,9 +4,19 @@ export default class Task extends Component {
 	render() {
 		return (
 			<div>
-				<h3>{this.props.task.title}</h3>
-				<div>{this.props.task.expr}</div>
-				<div>{this.props.task.eg}</div>
+				<div class="head">
+					<h3>{this.props.task.title}</h3>
+				</div>
+
+				<div className="body">
+				<div>
+					<span>{this.props.task.expr}</span>
+				</div>
+
+				<div>
+					<span>{this.props.task.eg}</span>
+				</div>
+				</div>
 			</div>
 		);
 	}
