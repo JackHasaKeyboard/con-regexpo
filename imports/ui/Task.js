896981945481a7c9040fa0d8af5c8234d99a8069
Jack Alma <jackhasakeyboard@gmail.com>
2017-11-23 23:03:08 -0700

    Add style and re-format further

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -4,18 +4,18 @@ export default class Task extends Component {
 	render() {
 		return (
 			<div>
-				<div class="head">
+				<div className="head">
 					<h3>{this.props.task.name}</h3>
 				</div>
 
 				<div className="body">
-				<div>
-					<span>{this.props.task.expr}</span>
-				</div>
+					<div>
+						<span>{this.props.task.expr}</span>
+					</div>
 
-				<div>
-					<span>{this.props.task.eg}</span>
-				</div>
+					<div>
+						<span>{this.props.task.eg}</span>
+					</div>
 				</div>
 			</div>
 		);
