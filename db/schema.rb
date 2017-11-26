263a8ffffb6b1ddcc7bc997187f3739123f82893
Jack Alma <jackhasakeyboard@gmail.com>
2017-11-25 23:23:53 -0700

    Add fields to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126060903) do
+ActiveRecord::Schema.define(version: 20171126061635) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "eg"
+    t.string "expr"
   end
 
 end
