2e77a73ac6c660c431a9c707d94d9ba05ad3c489
Jack Alma <jackhasakeyboard@gmail.com>
2017-11-30 10:33:41 -0700

    Add OAuth sign in through GitHub, courtesy: https://richonrails.com/articles/github-authentication-in-ruby-on-rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126061635) do
+ActiveRecord::Schema.define(version: 20171130171214) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 20171126061635) do
     t.string "name"
     t.string "eg"
     t.string "expr"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "avatar_url"
+    t.string "email"
+    t.string "uid"
+    t.string "provider"
+    t.string "oauth_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
