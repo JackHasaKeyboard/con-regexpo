c13169702281d1ec671f0a61329806863aceac59
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-12-04 19:25:18 -0700

    heh

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AsdfControllerTest < ActionDispatch::IntegrationTest
+class PostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
