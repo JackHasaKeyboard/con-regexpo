03696e5d027ed5afeb656f9437b376685c43ea8f
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-12-05 12:21:11 -0700

    Add Oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+	get "/auth/:provider/callback", to: "sessions#create"
+  get 'auth/failure', to: redirect('/')
+  delete 'signout', to: 'sessions#destroy', as: 'signout'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	resources :posts
