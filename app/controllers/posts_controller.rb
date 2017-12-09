48bda034f85dc49035c95e221d6085698dbd004e
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-12-08 21:37:02 -0700

    Add edit and delete buttons to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,7 @@ class PostsController < ApplicationController
 	# POST /posts.json
 	def create
 		@post = Post.new(post_params)
+		@post.user = current_user.id
 
 		respond_to do |format|
 			if @post.save
@@ -70,6 +71,6 @@ class PostsController < ApplicationController
 
 	# Never trust parameters from the scary internet, only allow the white list through.
 	def post_params
-		params.require(:post).permit(:name, :expr, :flav, :eg)
+		params.require(:post).permit(:name, :expr, :flav, :eg, :user)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201212937) do
+ActiveRecord::Schema.define(version: 20171209053219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171201212937) do
     t.string "eg"
     t.string "expr"
     t.string "flav"
+    t.integer "user"
   end
 
   create_table "users", force: :cascade do |t|
