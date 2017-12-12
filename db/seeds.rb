fd3b966f24413dd36ca5ca8444c305956820037e
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-12-12 16:28:10 -0700

    Add seed with 8 examples, courtesy: https://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149 with some things altered. Still a bit less than ideal

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create([
+	{name: 'Username', expr: '^[a-zA-Z0-9_-]{3,16}$', eg: 'JackHasaKeyboard'},
+	{name: 'Password', expr: '^[a-z0-9_-]{6,18}$', eg: 'myp4ssw0rd'},
+	{name: 'Hex', expr: '^#?[a-f0-9]{6}|[a-f0-9]{3}$', eg: '#a3c113'},
+	{name: 'Slug', expr: '^[a-z0-9-]+$', eg: 'my-title-here'},
+	{name: 'E-mail', expr: '(\A[\w+\-.]+)@([a-z\d\-]+)\.([a-z]+)\z', eg: 'jackhasakeyboard@gmail.com'},
+	{name: 'URL', expr: '^(https?):\/\/?www\.?([a-zA-Z0-9]+)?\.([a-zA-Z0-9]+)\.([a-z\.]{2,6})\/(.+)*\/?$', eg: 'https://www.regexpo.jackhasaskeyboard.com/url'},
+	{name: 'IP address', expr: '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', eg: '127.0.0.1'},
+	{name: 'HTML tag', expr: '<([a-z][a-z0-9]*)\b[^>]*>(.*?)</\1>', eg: '<h3 style="color: red">asdf</h3>'}
+])
